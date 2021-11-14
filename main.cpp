1c58216601a98af329ef8636857528a2b64a3b28
temporini <91788264+temporini@users.noreply.github.com>
2021-11-14 21:16:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
-}
+    int main() {
+
+        int n = 0, i =i, a = 0, b = 1,  int c;
+
+        cin >> n;
+
+        if (n >= 2) {
+            for ( i=0 ; i < n; i++){
+              
+                c = a + b;
+            cout << c << endl;
+            a = b;
+            b = c;
+        }
+        }else{
+            cout<<"errore"<<endl;
+        }
+
+
+        return 0;
+
+    }
